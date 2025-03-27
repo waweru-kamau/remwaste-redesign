61ac360e558228867960cc36fcb62fb8676de7ea
Waweru Kamau <wawerumail@gmail.com>
2025-03-27 22:24:26 +0300

    bug fix subheader on light mode

--- app/components/typography/Subheading.tsx
+++ app/components/typography/Subheading.tsx
@@ -3,7 +3,7 @@ import type { TextProps } from "./Heading";
 export function Subheading({ children, className }: TextProps) {
   return (
     <h2
-      className={`text-2xl font-bold tracking-tight text-white bottom-4 right-4 ${className}`}
+      className={`text-2xl font-bold tracking-tight text-black dark:text-white bottom-4 right-4 ${className}`}
     >
       {children}
     </h2>
