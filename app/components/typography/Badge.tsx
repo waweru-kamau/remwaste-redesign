76bec51a5d3e337ed4960c25e40f2479bdc888f5
Waweru Kamau <wawerumail@gmail.com>
2025-03-27 21:52:09 +0300

    updated badge colors

--- app/components/typography/Badge.tsx
+++ app/components/typography/Badge.tsx
@@ -1,14 +1,27 @@
 import type { TextProps } from "./Heading";
 
+const colorClasses = {
+  red: {
+    base: "bg-red-50 text-red-700 dark:text-red-400 dark:bg-red-500/10 ring-red-600/10",
+  },
+
+  orange: {
+    base: "bg-orange-50 text-orange-700 dark:text-orange-400 dark:bg-orange-500/10 ring-orange-600/10",
+  },
+} as const;
+
+type Color = keyof typeof colorClasses;
+
 export function Badge({
   children,
   className,
   color = "red",
-}: TextProps & { color?: string }) {
+}: TextProps & { color?: Color }) {
+  const colors = colorClasses[color];
+
   return (
     <span
-      className={`bg-${color}-50 dark:text-${color}-400 dark:bg-${color}-500/10 ring-1 ring-${color}-600/10 ring-inset 
-      inline-flex items-center gap-1 rounded-md px-2 py-1 text-xs font-normal text-red-700 w-fit justify-end self-end ${className}`}
+      className={`${colors.base} ring-1 ring-inset inline-flex items-center gap-1 rounded-md px-2 py-1 text-xs font-normal w-fit justify-end self-end ${className}`}
     >
       {children}
     </span>
